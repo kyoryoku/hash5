fba5f5d48a901680889be94afcc9a7c4ee5c5600
kyoryoku <kirino3@yandex.by>
2020-12-18 16:02:27 +0300

    1812_1602

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,11 @@
 ﻿using HandyControl.Data;
 using System;
 using System.Collections;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -21,38 +25,103 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        ObservableCollection<File> files = new ObservableCollection<File>();
+
         public MainWindow()
         {
             InitializeComponent();
-
-            ArrayList files = new ArrayList();
-            for (int i = 0; i < 10000; i++)
-            {
-                files.Add(new File("file_" + i + "_/asdasd/asdasdas/sadasdsad/asdasdsad/asdsadsad/sdaasdasdsa/asdsadsadasd/asdasd"));
-            }
-
-
-
             listView.ItemsSource = files;
+            lbl_filesCount.Content = "Файлов добавлено: " + files.Count.ToString();
+            
+            
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
             
         }
+        private void dragDrop_Drop(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // получаем строки объектов что нам дропнулись
+                string[] dropedObj = (string[])e.Data.GetData(DataFormats.FileDrop);
+
+                // проходим по каждому объекту и пытаемся вытасчить оттуда все файлы
+                foreach(string obj in dropedObj)
+                {
+                    if (Directory.Exists(obj)) {
+                        FindInDir(new DirectoryInfo(obj), "*", true);
+                    } else
+                    {
+                        files.Add(new File(obj));
+                    }
+                   
+                }
+            }
+            lbl_filesCount.Content = "Файлов добавлено: " + files.Count.ToString();
+        }
+
+        public void FindInDir(DirectoryInfo dir, string pattern, bool recursive)
+        {
+            foreach (FileInfo file in dir.GetFiles(pattern))
+            {
+                files.Add(new File(file.FullName));
+            }
+            if (recursive)
+            {
+                foreach (DirectoryInfo subdir in dir.GetDirectories())
+                {
+                    
+                    this.FindInDir(subdir, pattern, recursive);
+                }
+            }
+        }
     }
 
 
-    public class File
+
+    public class File : INotifyPropertyChanged
     {
-        public string fileName { get; set; }
-        public string hashValue { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private string _fileName;
+        public string fileName
+        {
+            get { return _fileName; }
+            set { SetProperty(ref _fileName, value); }
+        }
+
+        private string _hashValue;
+        public string hashValue
+        {
+            get { return _hashValue; }
+            set { SetProperty(ref _hashValue, value); }
+        }
+
+        protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (object.Equals(storage, value)) return false;
+
+            storage = value;
+            this.OnPropertyChanged(propertyName);
+            return true;
+        }
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            var eventHandler = this.PropertyChanged;
+            if (eventHandler != null)
+            {
+                eventHandler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
 
         public File(string fileName)
         {
             this.fileName = fileName;
-            this.hashValue = "00112233445566778899AABBCCDDEEFF00112233445566778899AABBCCDDEEFF";     
         }
+
     }
 
 
