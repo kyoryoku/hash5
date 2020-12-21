931398f07d806c2abbcb69d925ba60eb6a80dbed
kyoryoku <kirino3@yandex.by>
2020-12-21 15:20:59 +0300

    2112_1520

--- WpfApp1/App.xaml.cs
+++ WpfApp1/App.xaml.cs
@@ -14,7 +14,7 @@ namespace WpfApp1
     /// </summary>
     public partial class App : Application
     {
-   
+
     }
 
 
--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -6,7 +6,9 @@ using System.ComponentModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,9 +22,6 @@ using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         ObservableCollection<File> files = new ObservableCollection<File>();
@@ -36,10 +35,14 @@ namespace WpfApp1
             
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private async void btn1_click(object sender, RoutedEventArgs e)
         {
-            
+            foreach (File f in files)
+            {
+                f.calculateHash();
+            }
         }
+
         private void dragDrop_Drop(object sender, DragEventArgs e)
         {
             if (e.Data.GetDataPresent(DataFormats.FileDrop))
@@ -121,6 +124,67 @@ namespace WpfApp1
         {
             this.fileName = fileName;
         }
+
+
+
+
+        [DllImport("bee2.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void beltHashStart(byte[] state);
+
+        [DllImport("bee2.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void beltHashStepH(byte[] buf, int size, byte[] state);
+
+        [DllImport("bee2.dll", CallingConvention = CallingConvention.Cdecl)]
+        private static extern void beltHashStepG(byte[] hash, byte[] state);
+
+        private byte[] State1;
+        private byte[] Hash1;
+
+        async public void calculateHash()
+        {
+            //1. подготовка общих буферов
+            State1 = new byte[4096];
+            Hash1 = new byte[32];
+            for (int i = 0; i < 4096; i++){
+                State1[i] = 0;
+            }
+            for (int i = 0; i < 32; i++){
+                Hash1[i] = 0;
+            }
+            beltHashStart(State1);
+
+            //2. считываение файла и расчет блоков
+            await Task.Run(() => stepH());
+
+            //3. расчет окончательного значения и приведение к виду
+            beltHashStepG(Hash1, State1);
+            string res = "";
+            for (int i = 0; i < 32; i++){
+                if (Hash1[i] <= 15){
+                    res = res + "0" + Convert.ToString(Hash1[i], 16);
+                }
+                else{
+                    res = res + Convert.ToString(Hash1[i], 16);
+                }
+            }
+
+            this.hashValue = res;
+        }
+
+        private void stepH()
+        {
+            Console.Out.WriteLine(fileName + "  --->   " + Thread.CurrentThread.ManagedThreadId);
+            FileStream fstream = new FileStream(fileName, FileMode.Open);
+            int readSize = 0;
+            byte[] readBuff = new byte[4096];
+            while ((readSize = fstream.Read(readBuff, 0, 4096)) != 0)
+            {
+                beltHashStepH(readBuff, readSize, State1);
+            }
+            fstream.Close();
+        }
+
+        
 
     }
 
